c9cfe7a6cffb6e81f8629798cc2ee6db77042d7f
Liu Peibao <liupeibao@163.com>
2020-12-15 21:17:25 +0800

    samplefs: convert get_sb_nodev() to mount_nodev()
    commit 3c26ff6e499ee7e6f9f2bc7da5f2f30d80862ecf upstream
    convert get_sb_nodev() users
    
    Signed-off-by: Liu Peibao <liupeibao@163.com>

--- fs/samplefs/super.c
+++ fs/samplefs/super.c
@@ -44,10 +44,10 @@ struct super_block * samplefs_get_sb(struct file_system_type *fs_type,
 	return get_sb_nodev(fs_type, flags, data, samplefs_fill_super);
 }
 #else
-int samplefs_get_sb(struct file_system_type *fs_type,
-        int flags, const char *dev_name, void *data, struct vfsmount *mnt)
+static struct dentry *samplefs_mount(struct file_system_type *fs_type,
+	int flags, const char *dev_name, void *data)
 {
-	return get_sb_nodev(fs_type, flags, data, samplefs_fill_super, mnt);
+	return mount_nodev(fs_type, flags, data, samplefs_fill_super);
 }
 #endif
 
@@ -55,7 +55,7 @@ int samplefs_get_sb(struct file_system_type *fs_type,
 static struct file_system_type samplefs_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "samplefs",
-	.get_sb = samplefs_get_sb,
+	.mount = samplefs_mount,
 	.kill_sb = kill_anon_super,
 	/*  .fs_flags */
 };
